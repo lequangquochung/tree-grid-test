f23230d6aa06e97a4ab37b65b0cc742e7930a491
ducphuc-dev <55575330+ducphuc-dev@users.noreply.github.com>
2021-09-22 16:54:40 +0700

    feat/STDJ-010: Handle cut some rows event (#9)
    * [feat/STDJ-010] Init context menu
    
    * [feat/STDJ-010]: Fix reordering column
    
    * [feat/STDJ-010]: Handle cut some rows event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import {
   ContextMenuService,
   GridComponent,
   ContextMenuItemModel,
+  RowSelectEventArgs,
+  RowDataBoundEventArgs,
 } from '@syncfusion/ej2-angular-grids';
 import { TreeGrid, RowDD, Selection, Page, Resize, Reorder } from '@syncfusion/ej2-treegrid';
 import { MenuEventArgs } from '@syncfusion/ej2-navigations';
@@ -22,11 +24,13 @@ export class HomeComponent implements OnInit {
   quote: string | undefined;
   isLoading = false;
   public data = <any>[];
+  public dataWithoutNested:object[] = []
   public pageSettings: PageSettingsModel | undefined;
   public sortSettings: SortSettingsModel | undefined;
   public editSettings: EditSettingsModel | undefined;
   public toolbarOptions: ToolbarItems[] | undefined;
   public commands: CommandModel | undefined;
+  selectedRow:Array<any> = []
 
   // public contextMenuItems: ContextMenuItem[] = ['Copy', 'Edit', 'Delete', 'Save', 'Cancel', 'FirstPage', 'PrevPage','LastPage', 'NextPage'];
   public contextMenuItems: any = [
@@ -48,7 +52,7 @@ export class HomeComponent implements OnInit {
     { field: 'duration', headerText: 'Duration', textAlign: 'Right', width: '80' },
   ];
 
-  ngOnInit() {
+  ngOnInit() {    
     // Allow Drag / Drop to change order row
     TreeGrid.Inject(RowDD, Selection);
 
@@ -60,6 +64,7 @@ export class HomeComponent implements OnInit {
 
 
     this.data = sampleData;
+    this.getFullRecordWithoutNested(this.data)
     this.columns = [...this.dataColumn];
     this.pageSettings = { pageSize: 20 };
     // @ts-ignore
@@ -82,6 +87,32 @@ export class HomeComponent implements OnInit {
     if (args?.item?.id === 'copywithheader') {
       this.grid?.copy(true);
     }
+
+    if(args?.item?.id === 'cut') {
+      // let selectedrowindex: Array<any> = this.grid?.getSelectedRowIndexes() || [1]; 
+      const selectedrecords: object[] = this.grid?.getSelectedRecords() || []; 
+      this.selectedRow = selectedrecords
+      this.data = [...sampleData]
+    }
     return;
   }
+
+  rowBound(args: RowDataBoundEventArgs) {
+    const taskID:any = args?.data?.['taskID']
+    if(this.selectedRow.find(item => item.taskID === taskID)) {
+      // @ts-ignore
+      args?.row?.style?.background= '#f8d7da';
+    } 
+  }
+
+  
+  getFullRecordWithoutNested(data:object[]): any {
+    data.map((record:any) => {
+      this.dataWithoutNested.push(record)
+      if(record.subtasks && record.subtasks.length) {
+        this.getFullRecordWithoutNested(record.subtasks)
+      }
+    })
+  }
+
 }
