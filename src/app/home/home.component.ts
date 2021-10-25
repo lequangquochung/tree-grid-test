66b26b0479eb8838d7292ec0679a0e0496d40a46
lequangquochung <53163504+lequangquochung@users.noreply.github.com>
2021-10-25 10:47:20 +0700

    Fix/icon actions (#85)
    * feat/multi-sort
    
    * feat/multilsort - update display column name in checkbox form
    
    * fix/bug-task-022
    
    * fix/task-031-context-menu2
    
    * fix/task-033-multiselect-row
    
    * fix/delete-column
    
    * fix/check-delete-column
    
    * fix/check-delete-column-index
    
    * fix/check-delete-columnID
    
    * fix/multisort-remove
    
    * fix/add-icons-action
    
    * feat/merge edit and style column into 1
    
    * fix/add-icons
    
    * fix/add-icons
    
    Co-authored-by: LTheFourth <longpkd.dev@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -282,6 +282,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
     if (contextID === contextMenuID.togleFilter) {
       this.toggleFilter = !this.toggleFilter;
       _contextMenuItems[_contextMenuIndex].text = `Filter Columns ${this.toggleFilter ? `Off` : `On`}`;
+      _contextMenuItems[_contextMenuIndex].iconCss = `Filter Columns ${
+        this.toggleFilter ? `e-icons e-filter-clear` : `e-icons e-filter-3`
+      }`;
       this.contextMenuItems = [..._contextMenuItems];
 
       //change grid content height when togle filter
