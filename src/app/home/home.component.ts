7385c862c8d3b088b8665081f5a38433aac5a305
lequangquochung <53163504+lequangquochung@users.noreply.github.com>
2021-11-23 22:12:43 +0700

    Fix/context menu multi (#132)
    * feat/multi-sort
    
    * feat/multilsort - update display column name in checkbox form
    
    * fix/bug-task-022
    
    * fix/task-031-context-menu2
    
    * fix/task-033-multiselect-row
    
    * fix/delete-column
    
    * fix/check-delete-column
    
    * fix/check-delete-column-index
    
    * fix/check-delete-columnID
    
    * fix/multisort-remove
    
    * fix/add-icons-action
    
    * feat/merge edit and style column into 1
    
    * fix/add-icons
    
    * fix/add-icons
    
    * fix/icons
    
    * fix/test-icons
    
    * fix/icons-state
    
    * fix/test-iconstate
    
    * test icon state
    
    * test icon
    
    * test icon state
    
    * fix/change state text contextMenu
    
    * fix/ freeze column auto hide
    
    * fix/ add icon and change state text menucontext
    
    * fix/context menu header display when copy row
    
    * fix/show paste when copy/cut
    
    * fix/display menu text
    
    * fix/display menu text
    
    * update context menu, icon on/off, multiselect
    
    * fix state filter
    
    * fix state filter
    
    * fix context menu text when click
    
    * fix/change icon freeze column
    
    * fix/text multiselect
    
    * test context-menu ios
    
    * multiselect on
    
    * change checkbox icon
    
    * change icon context-menu
    
    * fix context-menu on/off
    
    * feat/change form create column, label contextmenu
    
    * fix create/edit column
    
    * fix multi-select mobile
    
    * fix/ set min-width table
    
    * fix/move multiseclect to menu-2
    
    Co-authored-by: LTheFourth <longpkd.dev@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -263,6 +263,75 @@ export class HomeComponent implements OnInit, AfterViewInit {
         this.scrollBackToLastPosition(lastScrollPosition);
       }, 500);
     }
+
+    // change multil select Of/Off
+    if (args.item.id === contextMenuID.multiSelect) {
+      this.toggleMultilSelect = !this.toggleMultilSelect;
+      const item = {
+        type: 'checkbox',
+        noEditor: true,
+        showInColumnChooser: false,
+        visible: false,
+        isRowSelector: true,
+      };
+      const dataCols = [...this.dataColumn];
+
+      if (!this.isMobileMode) {
+        if (!this.toggleMultilSelect) {
+          dataCols.splice(0, 1);
+          this.dataColumn = [...dataCols];
+          this.multiSelect.type = 'Single';
+        } else {
+          this.multiSelect.type = 'Multiple';
+          if (this.dataColumn[0]['type'] !== 'checkbox') {
+            this.dataColumn.splice(0, 0, item);
+          }
+        }
+        // this.columns = this.frozenColumns.concat(
+        //   this.dataColumn.filter(
+        //     (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
+        //   )
+        // );
+
+        if (this.frozenColumns.length > 0) {
+          this.isDropMode = false;
+        } else {
+          this.isDropMode = true;
+        }
+      } else {
+        if (!this.toggleMultilSelect) {
+          dataCols.splice(0, 1);
+          this.dataColumn = [...dataCols];
+          this.multiSelect.type = 'Single';
+          this.columns = this.frozenColumns.concat(
+            this.dataColumn.filter(
+              (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
+            )
+          );
+        } else {
+          this.multiSelect.type = 'Multiple';
+          if (this.dataColumn[0]['type'] !== 'checkbox') {
+            this.dataColumn.splice(0, 0, item);
+          }
+        }
+      }
+      this.columns = this.frozenColumns.concat(
+        this.dataColumn.filter(
+          (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
+        )
+      );
+
+      // _contextMenuItems[_contextMenuIndex].text = `Multil-Select ${this.toggleMultilSelect ? `Off` : `On`}`;
+      // this.contextMenuItems = [..._contextMenuItems];
+
+      this.isLoading = true;
+      const lastScrollPosition = this.getLastScrollPosition();
+      this.grid?.refresh();
+      setTimeout(() => {
+        this.isLoading = false;
+        this.scrollBackToLastPosition(lastScrollPosition);
+      }, 100);
+    }
   }
 
   columnContextClick(args: any) {
@@ -325,75 +394,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
       // this.toggleMultiSorting = !this.toggleMultiSorting;
       // _contextMenuItems[_contextMenuIndex].text = `Mutiple Sorting ${this.toggleMultiSorting ? `Off` : `On`}`;
       // this.contextMenuItems = [..._contextMenuItems];
-    }
-
-    // change multil select Of/Off
-    if (args.item.id === contextMenuID.multiSelect) {
-      this.toggleMultilSelect = !this.toggleMultilSelect;
-      const item = {
-        type: 'checkbox',
-        noEditor: true,
-        showInColumnChooser: false,
-        visible: false,
-        isRowSelector: true,
-      };
-      const dataCols = [...this.dataColumn];
-
-      if (!this.isMobileMode) {
-        if (!this.toggleMultilSelect) {
-          dataCols.splice(0, 1);
-          this.dataColumn = [...dataCols];
-          this.multiSelect.type = 'Single';
-        } else {
-          this.multiSelect.type = 'Multiple';
-          if (this.dataColumn[0]['type'] !== 'checkbox') {
-            this.dataColumn.splice(0, 0, item);
-          }
-        }
-        // this.columns = this.frozenColumns.concat(
-        //   this.dataColumn.filter(
-        //     (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
-        //   )
-        // );
-
-        if (this.frozenColumns.length > 0) {
-          this.isDropMode = false;
-        } else {
-          this.isDropMode = true;
-        }
-      } else {
-        if (!this.toggleMultilSelect) {
-          dataCols.splice(0, 1);
-          this.dataColumn = [...dataCols];
-          this.multiSelect.type = 'Single';
-          this.columns = this.frozenColumns.concat(
-            this.dataColumn.filter(
-              (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
-            )
-          );
-        } else {
-          this.multiSelect.type = 'Multiple';
-          if (this.dataColumn[0]['type'] !== 'checkbox') {
-            this.dataColumn.splice(0, 0, item);
-          }
-        }
-      }
-      this.columns = this.frozenColumns.concat(
-        this.dataColumn.filter(
-          (col) => this.frozenColumns.findIndex((f_col) => f_col.field == col.field) < 0 || col.isSelectRowCell
-        )
-      );
-
-      // _contextMenuItems[_contextMenuIndex].text = `Multil-Select ${this.toggleMultilSelect ? `Off` : `On`}`;
-      this.contextMenuItems = [..._contextMenuItems];
-
-      this.isLoading = true;
-      const lastScrollPosition = this.getLastScrollPosition();
-      this.grid?.refresh();
-      setTimeout(() => {
-        this.isLoading = false;
-        this.scrollBackToLastPosition(lastScrollPosition);
-      }, 100);
     }
   }
 
@@ -784,6 +784,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   actionBegin(args: any): void {
+    console.log(args);
     if (args.requestType == 'filtering' || args.requestType == 'infiniteScroll') {
       if (args.requestType == 'infiniteScroll') {
         this.loadedRecordCount += this.pageSettings.pageSize;
